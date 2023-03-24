f7388124ab9551ccfb671e09452eb8decf7256a1
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-03-24 14:13:43 -0400

    add search UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,21 @@ from '../components'
 export const Home = () => {
   const [loading,setLoading]=useState(false);
   const [allPosts,setAllPosts]=useState(null);
+  const [searchText, setSearchText]=useState('walls')
   
+  // fns compo
+const RenderCards = ({data,title}) => {
+  if(data.length>0){
+    return(
+      data.map((post)=><Card key={post._id} {...post}/>)
+    )
+  }
+  
+  return(
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+  );
+}
+
   return (
       <section className="max-w-7xl mx-auto">
       <div>
@@ -16,6 +30,34 @@ export const Home = () => {
       <div className="mt-16">
         <FormField/>
         </div>
+
+        <div className="mt-10">
+
+        {loading?(
+          <div className='flex justify-center items-center'>
+          <Loader/>
+        </div>):
+        (<>
+          {searchText && (
+            <h2 className="font-medium text-[#666e75] text-xl mb-3">
+            Showing Resuls for <span className="text-[#222328]">{searchText}</span>:
+            </h2>
+          ) }
+      {/* show images */}
+          <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+          { searchText?
+          (<RenderCards data={[]} title="No search results found"/>):
+          (<RenderCards data={[]} title="No Posts found" />)
+
+          }
+          </div>
+
+        </>)}
+        </div>
     </section>
   )
 }
+
+
+
+
