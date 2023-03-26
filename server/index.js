730c9b618adefadd6f28695be962ec15d564eed2
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-03-26 02:52:45 -0400

    added basic funs

--- server/index.js
+++ server/index.js
@@ -11,10 +11,11 @@ dotenv.config();
 
 // middlewares 
 
-
+const app=express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 // routes defined int ext file integartion
-const app=express()
-app.use(cors);
+
 //ext routes
 
 app.use('/api/v1/post',postRoutes)
@@ -22,13 +23,16 @@ app.use('/api/v1/post',postRoutes)
 app.use('/api/v1/dalle',dalleRoutes)
 
 
+
+
+
 // middleware function in Express starting from v4.16.0. It parses incoming JSON requests and puts the parsed data in req.body
-app.use(express.json());
+// app.use(express.json());
 
 // puts the parsed data in req.body
 app.post('/test', function(req,res) {
     // Without `express.json()`, `req.body` is undefined.
-    console.log(`${req.body}`);
+    // console.log(`${req.body}`);
   });
 
   const PORT=8080;
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
-import express  from "express"
+import express  from 'express'
 import * as dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from "openai"
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config();
 
@@ -13,20 +13,18 @@ dotenv.config();
 const router=express.Router();
 
 const configuration=new Configuration({
-organization: "org-TlwctFF8eZqaR3QFOq42Glig",
     apiKey: process.env.OPENAI_API_KEY,
 });
+console.log(configuration.apiKey);
 const openai= new OpenAIApi(configuration);
-console.log(openai)
+// console.log(openai)
 router.get('/',(req,res)=>{
     res.send("Get from POST_ROUTE");
 })
 
-
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    console.log(prompt)
 
     const aiResponse = await openai.createImage({
       prompt,
@@ -36,14 +34,13 @@ router.route('/').post(async (req, res) => {
     });
 
     const image = aiResponse.data.data[0].b64_json;
+    console.log(image)
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
   }
 });
-
-
 
 export default router;
 
